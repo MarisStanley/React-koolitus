387159423c7274288dca377cc20a4f529eeffb76
MarisStanley <131513249+MarisStanley@users.noreply.github.com>
2023-04-22 16:12:07 +0300

    Kodutöö 1/3

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,18 @@
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="nupp">Nupp</button>
+      <h3 className="tekst">Laps segab koguaeg!</h3>
+      <h3 className="tekst1">Laps segab koguaeg vol2!</h3>
+      <body className='pruun'/>
+
+      <img className="pilt" src="https://media.makeameme.org/created/am-i-crazy-5b28a3.jpg" />
+    </div>
+  );
+}
+
+export default App;
